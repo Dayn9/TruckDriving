9cae80a774c6ccb3c67e751cc17cd0dc6a94177f
Dane Sherman <danemsherman@gmail.com>
2017-12-04 20:01:24 -0500

    fix repos

---
+++ CarTest/Assets/scripts/WheelSet.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class WheelSet : System.Object {
+    [SerializeField] public WheelCollider rightWheelCollider;
+    [SerializeField] public GameObject rightWheel;
+    [SerializeField] public WheelCollider leftWheelCollider;
+    [SerializeField] public GameObject leftWheel;
+    [SerializeField] public bool driveWheel;
+    [SerializeField] public bool steerWheel;
+}
